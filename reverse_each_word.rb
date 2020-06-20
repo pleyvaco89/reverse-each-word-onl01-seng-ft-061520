d6acb08c47248829e9219e0c5857137e1f747b3f
Priscilla Leyva <pleyva.co89@gmail.com>
2020-06-20 07:35:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,3 @@
 def reverse_each_word(array)
-  new_array = array.split(" ")
-  reveresed _array = new_Array.each {|x| x.reverse!}
-  return reversed_array.join(" ")
+  array.split(" ").collect {|x| x.reverse!}.join(" ")
 end
